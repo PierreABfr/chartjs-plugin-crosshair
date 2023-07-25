2ac4d9b54e69efc09da3d08610efa70756800033
Pierre-A B <15235171+PierreABfr@users.noreply.github.com>
2023-07-25 13:38:21 +0200

    cancel if crosshair plugin is not used

--- src/trace.js
+++ src/trace.js
@@ -257,7 +257,7 @@ export default {
 
   afterDraw: function(chart) {
 
-    if (!chart.crosshair.enabled) {
+    if (chart.crosshair == undefined || !chart.crosshair.enabled) {
       return;
     }
 
